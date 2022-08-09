90a3460e1cbc90c2c140b825f91e47e908c3fd78
Saheel Das <saheelkumar.das@bajajfinserv.in>
2022-08-09 13:12:26 +0530

    created page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,44 @@
 import './App.css';
+import logo from "./assets/logo.png";
+import Page from './components/page';
+import { useState } from "react";
+import { pageData } from "./utils/constants";
+import { IActivePage } from './utils/interfaces';
 
-function App() {
+const App = () => {
+  const [activePage, setActivePage] = useState(0);
+  const totalPages = pageData.length;
+  const handleCtaClick = () => {
+    if(activePage < totalPages - 1) {
+      setActivePage(activePage + 1);
+    }
+  }
   return (
-    <div className="App">
-      Hello
+    <div className="root">
+      <div className="logo">
+        <img src={logo} alt="logo" height={40} width={40} />
+        <h1>Eden</h1>
+      </div>
+      <div className="stepper">
+        {pageData.map((page, index) => <Stepper activePage={activePage} index={index} totalPages={totalPages} />)}
+      </div>
+      {pageData.map((page, index) => <>{ activePage === index ? <Page key={String(index)} {...page} handleCtaClick={handleCtaClick} /> : <></>}</>)}
     </div>
   );
+}
+
+const Stepper = ({ activePage, index, totalPages }: IActivePage) => {
+  const getStyles = () => {
+    if(index === activePage) return "activeConnector";
+    if(index  < activePage) return "completed";
+    return "";
+  }
+  return (
+    <>
+     <div className={`${activePage >= index && "active"} step`}>{index + 1}</div>
+     {index < totalPages - 1 && <span className={`${getStyles()} connector`} />}
+    </>
+  )
 }
 
 export default App;
